e5cc4117dcc236ace073334cd5826965fe21aea3
YanaS <yana@gmail.com>
2017-02-11 12:02:27 +0200

    refactored Listener

--- src/main/java/com/vertex/academy/homework2/Listener.java
+++ src/main/java/com/vertex/academy/homework2/Listener.java
@@ -9,7 +9,17 @@ import java.util.*;
 public class Listener {
     public static void main(String[] args) {
 
-         Comparator<People> comparator = (o1, o2) -> {
+        PriorityQueue<People> customerPriorityQueue = new PriorityQueue<>(getComparator());
+        for (int i = 0; i < Math.random() * 100; i++) {
+            customerPriorityQueue.add(new Man());
+            customerPriorityQueue.add(new Woman());
+        }
+        while (!customerPriorityQueue.isEmpty())
+            System.out.println(customerPriorityQueue.poll());
+    }
+
+    private static Comparator<People> getComparator() {
+        Comparator<People> comparator = (o1, o2) -> {
             if ((o1 instanceof Man) && (o2 instanceof Man) || (o1 instanceof Woman) && (o2 instanceof Woman)) {
                 if (o1.getProperties() < o2.getProperties()) {
                     return 1;
@@ -20,18 +30,8 @@ public class Listener {
                 return 1;
             } else return -1;
         };
-
-        PriorityQueue<People> customerPriorityQueue = new PriorityQueue<>(comparator);
-        for (int i = 0; i < 5; i++) {
-            customerPriorityQueue.add(new Man());
-            customerPriorityQueue.add(new Woman());
-        }
-
-     while (!customerPriorityQueue.isEmpty())
-         System.out.println(customerPriorityQueue.poll());
-
+        return comparator;
     }
-
 
 }
 
